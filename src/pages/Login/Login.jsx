0500b74b4df9b5585b4dd2233d32ffa3c619d9d4
Evarmedia <mishakmanuel@gmail.com>
2024-03-24 12:33:20 +0100

    updated login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,8 +8,9 @@ const Login = () => {
         <img
           className="mx-auto h-10 w-auto"
           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
+          alt="CozyCove"
         />
+        <h1>WELCOME TO COZYCOVE</h1>
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
         </h2>
